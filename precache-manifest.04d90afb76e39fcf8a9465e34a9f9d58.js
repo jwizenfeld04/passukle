20a604b159c974f02589f8d816de990614ef38d3
Jeremy Wizenfeld <jeremywizenfeld@Jeremys-MacBook-Air.local>
2023-03-19 01:35:33 +0200

    Updates

---
+++ precache-manifest.04d90afb76e39fcf8a9465e34a9f9d58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f248216e1dd519a26c",
+    "url": "/passukle/static/css/main.21beaa4a.chunk.css"
+  },
+  {
+    "revision": "72f248216e1dd519a26c",
+    "url": "/passukle/static/js/main.72f24821.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/passukle/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "b0cadf5870f1549ee06c",
+    "url": "/passukle/static/js/2.b0cadf58.chunk.js"
+  },
+  {
+    "revision": "8e8198f3048c05336745",
+    "url": "/passukle/static/js/runtime~main.8e8198f3.js"
+  },
+  {
+    "revision": "d40400c3ceb7dee32ba9adcc6a320aa1",
+    "url": "/passukle/static/media/pesukim-data.d40400c3.csv"
+  },
+  {
+    "revision": "042ae457dbb0e6b9ee2ea865c6d31049",
+    "url": "/passukle/static/media/parsha-list.042ae457.txt"
+  },
+  {
+    "revision": "9c2793b7b9d79d7784e2bf567f76ad7e",
+    "url": "/passukle/index.html"
+  }
+];
